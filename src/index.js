1249d2e60aedf4815620f0e74759923885178c6d
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-04-12 12:42:06 -0400

    Button
    Added a button that updates the text on the screen

--- src/index.js
+++ src/index.js
@@ -5,18 +5,26 @@ import "./styles.css";
 
 class Example extends React.Component {
   state = {
-    text: ""
+    text: "",
+    bubble: ""
   };
 
   takeText = event => {
     this.setState({ text: event.target.value });
   };
 
+  makeChain = () => {
+    this.setState({ bubble: this.state.text });
+  };
   render() {
     return (
-      <>
+      <div className="App">
         <input type="text" value={this.state.text} onChange={this.takeText} />
-      </>
+        <button className="newChainBtn" onClick={this.makeChain}>
+          Create New Chain
+        </button>
+        <div className="textBubble">{this.state.bubble}</div>
+      </div>
     );
   }
 }
