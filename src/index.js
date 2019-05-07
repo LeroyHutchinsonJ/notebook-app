763f935754f157b81962918554a1631d5ac63975
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-05-07 17:20:04 -0400

    Comments
    Added some comments to make the code more readable

--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from "react-dom";
 import Words from "./words";
 import "./styles.css";
 
-class Bev extends React.Component {
+//Creates a class
+class App extends React.Component {
+  //Adds state to the class, a string and an array
   state = {
     text: "",
     bubble: []
   };
+  //Sets a counter to use for the array value id
   counter = 0;
 
+  //Function that takes text that is inputed, and sets it to text in state
+  takeText = event => {
+    this.setState({ text: event.target.value });
+  };
+
+  //Creates a function that generates a value and id for each member of the array, the value would be whatever is inside of text
   makeBubble = () => {
     return {
       id: this.counter++,
@@ -17,10 +26,7 @@ class Bev extends React.Component {
     };
   };
 
-  takeText = event => {
-    this.setState({ text: event.target.value });
-  };
-
+  //Function that adds the ID and Value generated to the bubble array
   addBubble = () => {
     this.setState({ bubble: [...this.state.bubble, this.makeBubble()] });
   };
@@ -40,4 +46,4 @@ class Bev extends React.Component {
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Bev />, rootElement);
+ReactDOM.render(<App />, rootElement);
