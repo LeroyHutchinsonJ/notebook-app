d2b9e7b0accbed393b7b5e1ec889d247370c9cc2
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-05-07 00:00:09 -0400

    Class
    Added a classname to the unordered list

--- src/words.js
+++ src/words.js
@@ -3,7 +3,7 @@ import React from "react";
 class Words extends React.PureComponent {
   render() {
     return (
-      <ul>
+      <ul className="chainList">
         {this.props.words.map(sentence => (
           <li id={sentence.counter}>{sentence.value}</li>
         ))}
